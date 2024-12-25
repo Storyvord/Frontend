ad205cf1c01efff11aed98fffc3384110be77cf0
Souvik <souvik@storyvord.com>
2024-12-25 23:29:56 +0530

    fix css issue on FeaturesSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col max-w-[2000px] mx-auto">
       <SiteHeader />
-      <main className="flex-1 px-6 lg:px-10 xl:px-28">
+      <main className="flex-1 px-4 md:px-6 lg:px-10 xl:px-28">
         <HeroSection />
         <AiSimplifies />
         <FeaturesSection />
